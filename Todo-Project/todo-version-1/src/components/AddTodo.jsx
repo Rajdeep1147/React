0e0b0f4d666c4bb31cac314f4abf88502af31f74
Rajdeep1147 <rajdeep@lawsikho.in>
2025-11-20 12:09:53 +0530

    Changes

--- Todo-Project/todo-version-1/src/components/AddTodo.jsx
+++ Todo-Project/todo-version-1/src/components/AddTodo.jsx
@@ -3,10 +3,10 @@ function AddTodo() {
     <div className="container text-center">
       <div className="row kg-row">
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo Here"></input>
+          <input type="text" placeholder="Enter Todo Here" className="form-control"></input>
         </div>
         <div className="col-4">
-          <input type="date" />{" "}
+          <input type="date" className="form-control" />{" "}
         </div>
         <div className="col-2">
           <button type="button" className="btn btn-success kg-button">
