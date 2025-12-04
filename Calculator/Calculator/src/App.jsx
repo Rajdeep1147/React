961ae98a069e4c523451fdd4e47ab774c43ac0f8
Rajdeep1147 <rajdeep@lawsikho.in>
2025-12-04 12:53:09 +0530

    Calculator Project Completed

--- Calculator/Calculator/src/App.jsx
+++ Calculator/Calculator/src/App.jsx
@@ -2,12 +2,27 @@ import { Button } from "bootstrap";
 import styles from "./App.module.css";
 import Display from "./components/Display";
 import ButtonContainer from "./components/ButtonContainer";
+import { useState } from "react";
 
 function App() {
+  const [calval, setCalval] = useState("");
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalval("");
+      return;
+    } else if (buttonText === "=") {
+      let result = eval(calval);
+      setCalval(result);
+    } else {
+      console.log(calval + buttonText);
+      setCalval(calval + buttonText);
+    }
+  };
+
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonContainer />
+      <Display displayValue={calval} />
+      <ButtonContainer onButtonClick={onButtonClick} />
     </div>
   );
 }
--- Calculator/Calculator/src/components/ButtonContainer.jsx
+++ Calculator/Calculator/src/components/ButtonContainer.jsx
@@ -1,6 +1,6 @@
 import styles from "./ButtonContainer.module.css";
 
-const ButtonContainer = () => {
+const ButtonContainer = ({ onButtonClick }) => {
   const buttonsNames = [
     "C",
     "1",
@@ -10,19 +10,24 @@ const ButtonContainer = () => {
     "4",
     "5",
     "6",
-    "*",
+    "+",
     "7",
     "8",
     "9",
     "=",
     "/",
     "0",
+    "*",
     ".",
   ];
   return (
     <div className={styles.buttonContainer}>
       {buttonsNames.map((btnName) => (
-        <button key={btnName} className={styles.button}>
+        <button
+          key={btnName}
+          className={styles.button}
+          onClick={() => onButtonClick(btnName)}
+        >
           {btnName}
         </button>
       ))}
